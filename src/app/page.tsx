62ba09698b3c980890438af44883fb8ad9684fa6
hp <uk129195@gmail.com>
2025-01-02 15:22:11 +0500

    Your commit message here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,6 @@ export default function Home() {
       <h3 className="text-4xl">IBM</h3>
       </div>
     </div>
-    <BlogPage/>
 
     <div className="flex flex-col gap-4 items-center mt-32 justify-center h-[450px] bg-[#5001ab]">
         <h2 className="text-5xl font-semibold text-white lg:px-32 text-center">
